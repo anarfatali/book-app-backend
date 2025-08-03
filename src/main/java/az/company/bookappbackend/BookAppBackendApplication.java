2a6887d71fe8d5bb6edeb2b8e8744bed7916f0a7
anarouis <anarftliyev@gmail.com>
2025-08-03 18:56:39 +0400

    feat: initial commit

---
+++ src/main/java/az/company/bookappbackend/BookAppBackendApplication.java
@@ -0,0 +1,13 @@
+package az.company.bookappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/company/bookappbackend/BookAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.bookappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
